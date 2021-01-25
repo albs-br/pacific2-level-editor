9a269ad4fbf4513c1f4d30894e3a253242f9b0ab
albs-br <albs_br@yahoo.com.br>
2021-01-24 23:54:55 -0300

    v.0.2.0

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # pacific2-level-editor
 
+Web based level editor for Pacific 2 game for MSX 
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pacific2-level-editor)
--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 // Import stylesheets
 import "./style.css";
 
-// Write Javascript code!
 const appDiv = document.getElementById("app");
-//appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const COLUMNS = 32;
 const LINES = 23;
 
-// appDiv.innerHTML += '<table>';
+const borderMouseOver = "1px solid blue";
 
-// for(let j=0; j < LINES; j++) {
-//   appDiv.innerHTML += '  <tr>';
-//   for(let i=0; i < COLUMNS; i++) {
-//     appDiv.innerHTML += '    <td>sdfsdf</td>';
-//   }
-//   appDiv.innerHTML += '  </tr>';
-// }
-
-// appDiv.innerHTML += '</table>';
+const withBorder = "1px solid lightgray";
+const noBorder = "1px solid white";
+let currentBorder = withBorder;
 
 var table = document.createElement("table");
 table.style.borderSpacing = "0px";
 table.style.borderPadding = "0px";
-table.style.border = "solid 1px lightgray";
-
-
+table.style.border = "1px solid lightgray";
 
 for (var i = 0; i < LINES; i++) {
   var tr = document.createElement("tr");
@@ -34,12 +24,20 @@ for (var i = 0; i < LINES; i++) {
     var td = document.createElement("td");
     td.style.width = "20px";
     td.style.height = "20px";
-    td.style.border = "solid 1px lightgray";
+    td.style.border = "1px solid lightgray";
     td.addEventListener("mouseover", function() {
-      this.style.border = "solid 1px blue";
+      $(this).css({
+        border: borderMouseOver
+      }); 
     });
     td.addEventListener("mouseout", function() {
-      this.style.border = "solid 1px lightgray";
+      $(this).css({
+        border: currentBorder
+      }); 
+    });
+    td.addEventListener("click", function() {
+      this.style.backgroundColor = "red";
+      this.style.border = "0px solid green";
     });
     //var text = document.createTextNode('');
     //td.appendChild(text);
@@ -49,3 +47,17 @@ for (var i = 0; i < LINES; i++) {
   table.appendChild(tr);
 }
 app.appendChild(table);
+
+document.all["chkGrid"].addEventListener("click", function() {
+  console.info(this.checked);
+
+  if (this.checked) {
+    currentBorder = withBorder;
+  } else {
+    currentBorder = noBorder;
+  }
+
+  $("td").css({
+    border: currentBorder
+  });  
+});
